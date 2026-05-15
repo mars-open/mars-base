df933ccdd0071d940c431e6d7ddbc1f97c3fb118
Zacharias Kull <zacharias.kull@elca.ch>
2026-05-15 23:05:15 +0200

    improve osm export

--- data/ext/osm/export.sh
+++ data/ext/osm/export.sh
@@ -2,17 +2,24 @@
 
 inputfile=$1
 region=$( [[ $inputfile =~ ^([a-zA-Z0-9]+) ]] && echo "${BASH_REMATCH[1]}" )
-outputfile="$region-railway-tracks.jsons"
+outputfileTracks="$region-railway-tracks.jsons"
+outputfileSwitches="$region-railway-switches.jsons"
 echo .
-echo "creating output file $outputfile..."
-echo .
-
+echo "creating output file $outputfileTracks..."
 echo "Filtering railway tracks from OSM data..."
-osmium tags-filter $inputfile w/railway=rail,tram,subway,narrow_gauge --overwrite -o railway.pbf
-echo .
-
+osmium tags-filter $inputfile "w/railway=rail,tram,subway,narrow_gauge" --overwrite -o railway.pbf
 echo "Exporting filtered railway tracks to GeoJSON sequence format..."
-osmium export railway.pbf --geometry-types linestring -f geojsonseq --format-option=print_record_separator=false --overwrite -o $outputfile -e
+osmium export railway.pbf -c export-config.json --geometry-types linestring -f geojsonseq --format-option=print_record_separator=false --overwrite -o $outputfileTracks -e
+echo "done"
+
+rm railway.pbf
+
+echo .
+echo "creating output file $outputfileSwitches..."
+echo "Filtering railway switches from OSM data..."
+osmium tags-filter $inputfile "n/railway=switch" --overwrite -o railway.pbf
+echo "Exporting filtered railway tracks to GeoJSON sequence format..."
+osmium export railway.pbf -c export-config.json --geometry-types point -f geojsonseq --format-option=print_record_separator=false --overwrite -o $outputfileSwitches -e
 echo "done"
 
 rm railway.pbf
